efc38abb2f3da1dc16d42f6f5cf0b7065c96fce8
Billy Harkins <17248+wwharkins@users.noreply.github.tamu.edu>
2024-02-28 08:21:25 -0600

    Added thumbnail support and API result support

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -1,5 +1,6 @@
 import { IResult } from "./IResult"
 import React, { useState } from "react";
+import Image from "next/image";
 
 type Props = {
     videoInfo: IResult;
@@ -20,17 +21,44 @@ const timestampConversion = (time:number) => {
     }
 }
 
+const snippetConversion = (snippet: string) => {
+    let re = /<mark>/gi;
+    let index = snippet.search(re);
+
+    let re2 = /<\/mark>/gi;
+    let index2 = snippet.search(re2);
+
+    return (
+        <div>
+            {snippet.substring(0, index)}
+            <b>{snippet.substring(index + 6, index2)}</b>
+            {snippet.substring(index2 + 7)}
+        </div>
+    );
+}
+
 const Card = ({videoInfo}: Props) => {
+    const thumbnailLink = "https://i.ytimg.com/vi/" + videoInfo.video_id + "/mqdefault.jpg";
     return (
         <div className="border rounded border-gray-500 p-2 m-2">
-            <p>Thumbnail</p>
+            <Image
+                    className="relative w-auto"
+                    src={thumbnailLink}
+                    alt="Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
             <p className="text-xl font-bold">{videoInfo.title}</p>
             <p className="italic">{videoInfo.channel_name}</p>
-
-            <div className="flex flex-row">
+            
+            <div className="flex flex-col">
                 {videoInfo.matches.map((result, index) => {
                     return(
-                        <p key={index}>{timestampConversion(result.timestamp) + " " + result.snippet}</p>
+                        <div key={index} className="my-3">
+                            <a href={"https://youtu.be/" + videoInfo.video_id + "?t=" + result.timestamp} className="font-bold text-blue-500">{timestampConversion(result.timestamp)}</a>
+                            {snippetConversion(result.snippet)}
+                        </div>
                     )
                 })
                 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import Image from "next/image";
 import { IResult, IMatches } from "./IResult";
 import React, { useState } from "react";
 import Card from "./card";
@@ -7,8 +6,6 @@ import Card from "./card";
 const apiLink = "https://us-central1-scriptsearch.cloudfunctions.net/transcript-api"
 
 export default function Home() {
-    const [resultsReceived, setResultsReceived] = useState(false);
-    const [results, setResults] = useState("");
     const [searchResults, setSearchResults] = useState<IResult[]>([]);
 
     const backendConnect = () => {
@@ -32,7 +29,6 @@ export default function Home() {
         })
             .then(data => {
                 console.log(data);
-                setResultsReceived(true);
                 setSearchResults(data["hits"]);
             })
     }
@@ -68,14 +64,6 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"> */}
             <div className="">    
-                {/* <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] w-auto h-auto"
-                    src="/nextjs-github-pages/Youtube Scripter Logo Big.jpg"
-                    alt="Logo"
-                    width={180}
-                    height={37}
-                    priority
-                /> */}
                 <p className="text-2xl before:content-['ScriptSearch'] before:text-red-500 before:font-bold before:"> - YouTube Transcript Search</p>
             </div>
 
@@ -97,12 +85,6 @@ export default function Home() {
                 })
                 }
             </div>
-
-            {resultsReceived &&
-                <div className="my-20 p-1">
-                    <p className="font-bold text-xl">Query: {results}</p>
-                </div>
-            }
         </main>
     );
 }
