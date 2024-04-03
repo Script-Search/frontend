c49bc365e209044491f2f953cddede453d3954b1
Billy Harkins <17248+wwharkins@users.noreply.github.tamu.edu>
2024-04-03 15:42:24 -0500

    Added animations to modal view transition

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -102,25 +102,23 @@ const Card = ({videoInfo}: Props) => {
                 <p className="italic">{videoInfo.channel_name}</p>
             </div> 
 
-        {modal &&
-                <dialog onClick={handleOverlayClick} className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 overflow-auto z-50 backdrop-blur flex justify-center items-center">
-                    <div id = "modal" className="bg-white m-auto px-8 py-4 border-8 border-red-600 rounded-lg flex flex-col flex-wrap dark:bg-black dark:text-white w-1/3">
-                        <p id = "modal" className="font-bold text-2xl place-self-center">Results</p>
-                            {videoInfo.matches.map((result, index) => {
-                                return (
-                                    <div id = "modal" key={index} className="m-3">
-                                        <a id = "modal" href={`https://youtu.be/${videoInfo.video_id}?t=${result.timestamp}`} target="_blank" className="font-bold text-blue-500">
-                                        {timestampConversion(result.timestamp)}
-                                        </a>
-                                        <span id = "modal"> {createMarkup(result.snippet)} </span>
-                                    </div>
-                                );                    
-                            })}
-                            <br/>
-                            <button type="button" onClick={closeModal} className="bg-red-600 text-white p-2 border border-red-700 rounded-lg transition-all ease-in-out duration-100 hover:font-bold hover:ring-4 hover:ring-red-700 ">Close</button>
-                    </div>
+            <dialog onClick={handleOverlayClick} className={`fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 overflow-auto z-50 backdrop-blur flex justify-center items-center transition-all ease-in-out duration-300 ${modal ? 'visible opacity-100' : 'invisible opacity-0'}`}>
+                <div id = "modal" className="bg-white m-auto px-8 py-4 border-8 border-red-600 rounded-lg flex flex-col flex-wrap dark:bg-black dark:text-white w-1/3">
+                    <p id = "modal" className="font-bold text-2xl place-self-center">Results</p>
+                    {videoInfo.matches.map((result, index) => {
+                        return (
+                            <div id = "modal" key={index} className="m-3">
+                                <a id = "modal" href={`https://youtu.be/${videoInfo.video_id}?t=${result.timestamp}`} target="_blank" className="font-bold text-blue-500">
+                                {timestampConversion(result.timestamp)}
+                                </a>
+                                <span id = "modal"> {createMarkup(result.snippet)} </span>
+                            </div>
+                        );                    
+                    })}
+                    <br/>
+                    <button type="button" onClick={closeModal} className="bg-red-600 text-white p-2 border border-red-700 rounded-lg transition-all ease-in-out duration-100 hover:font-bold hover:ring-4 hover:ring-red-700 ">Close</button>
+                </div>
             </dialog>
-        }
         </div>
     )   
 };
